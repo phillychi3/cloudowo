d798f9d27ad1f5c81d4c04e1eb8a6f91e3c4ca20
phillychi3 <phillychi3@gmail.com>
2025-07-25 17:47:36 +0800

    temp

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+<div>
+	{#if page?.error}
+		<div class="mt-4 p-4">
+			{#if page?.status}
+				<p>Page status: {page?.status}</p>
+				<img src="https://http.cat/{page?.status}" alt="HTTP status code" />
+			{/if}
+			{#if page?.error?.message}
+				<p>Error message: {page?.error?.message}</p>
+			{/if}
+		</div>
+	{/if}
+</div>
---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,91 @@
+<svelte:head>
+	<title>Whitecloud's Resume</title>
+</svelte:head>
+
+<div class="p-20 text-[#dfe6e9]">
+	<h1 class="mb-4 text-2xl font-bold">Whitecloud</h1>
+	<div>
+		<p>about me ...</p>
+	</div>
+
+	<div class="mt-4">
+		<h2 class="text-xl">Skill</h2>
+		<div class="mt-2">
+			<h3 class="text-lg">Programming Languages</h3>
+			<div class="mt-2">
+				<ul class="list-disc pl-8">
+					<li>JavaScript</li>
+					<li>TypeScript</li>
+					<li>Python</li>
+					<li>Rust</li>
+					<li>Go</li>
+				</ul>
+			</div>
+		</div>
+		<div class="mt-2">
+			<h3 class="text-lg">Frameworks and Libraries</h3>
+			<div class="mt-2">
+				<ul class="list-disc pl-8">
+					<li>Svelte</li>
+					<li>SvelteKit</li>
+					<li>React</li>
+					<li>Vue 3</li>
+					<li>Nuxtjs</li>
+					<li>Node.js</li>
+					<li>Express</li>
+					<li>Flask</li>
+					<li>Fastapi</li>
+					<li>MongoDB</li>
+					<li>MySQL</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="mt-4">
+		<h2 class="text-xl">Education</h2>
+		<ul class="list-disc pl-8">
+			<li class="mt-2">
+				<p>Department of Information Management</p>
+				<p>National Yunlin University of Science and Technology, 2022-2026</p>
+			</li>
+		</ul>
+	</div>
+
+	<div class="mt-4">
+		<h2 class="text-xl">Work Experiences</h2>
+		<ul class="list-disc pl-8">
+			<li class="mt-2">
+				<strong class="pr-2">2025-now</strong><span>LaplaceAI</span>
+			</li>
+		</ul>
+	</div>
+
+	<div class="mt-4">
+		<h2 class="text-xl">Community</h2>
+		<ul class="list-disc pl-8">
+			<li class="mt-2">KCD Taipei 2025</li>
+		</ul>
+	</div>
+
+	<div class="mt-4">
+		<h2 class="text-xl">Contest</h2>
+		<ul class="list-disc pl-8">
+			<li class="mt-2">55 全國技能中區分區賽 網路安全 佳作</li>
+		</ul>
+	</div>
+
+	<div class="mt-4">
+		<h2 class="text-xl">Contact Me</h2>
+		<p>cloud@cloudowo.com</p>
+	</div>
+</div>
+
+<style>
+	:global(html) {
+		background-color: #2d3436;
+	}
+	strong {
+		color: #f5f6fa;
+	}
+</style>
