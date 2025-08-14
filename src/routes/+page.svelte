5a1a5ff6022a053480be03488506736b9160a43a
phillychi3 <phillychi3@gmail.com>
2025-08-15 00:11:31 +0800

    sync

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>網站首頁 大字介紹與滾動提示</div>
+
+<div>
+    關於各種顏色的雲們...
+</div>
+
+<div>
+    技能
+</div>
+
+<div>
+    作品集
+</div>
+
+<div>
+    興趣等..
+</div>
+
+<div>
+    底部 聯絡方式
+</div>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+	import { Github } from 'svelte-github-card';
+
+	let { data }: PageProps = $props();
+</script>
+
+<section>
+	<div>
+		<Github repository="https://github.com/cl0udlab/Lithium-player"></Github>
+	</div>
+</section>
