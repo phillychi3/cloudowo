af780962b119f333bdab1ee33c5a573af6d7542d
phillychi3 <phillychi3@gmail.com>
2025-10-07 01:11:58 +0800

    format:

--- src/routes/projects/LithiumPlayer/+page.svelte
+++ src/routes/projects/LithiumPlayer/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-    import type { PageProps } from './$types';
+	import type { PageProps } from './$types';
 
-    let { data }: PageProps = $props();
+	let { data }: PageProps = $props();
 </script>
+
 <p>還沒</p>
